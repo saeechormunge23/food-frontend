fc49ebf55054b06da7d56e0a76eac89de5e402f1
saeechormunge23 <111693070+saeechormunge23@users.noreply.github.com>
2024-11-15 15:29:47 +0530

    Update ProfileModal.js

--- Frontend/src/components/ProfileModal.js
+++ Frontend/src/components/ProfileModal.js
@@ -6,22 +6,22 @@ const ProfileModal = ({ show, handleClose, user }) => {
   const [orders, setOrders] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchOrders = async () => {
-      try {
-        setLoading(true);
-        const response = await orderService.getOrdersByUser(user?.userId);
-        console.log(response);
-        setOrders(response.data);
-      } catch (error) {
-        console.error("Error fetching orders:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchOrders = async () => {
+  //     try {
+  //       setLoading(true);
+  //       const response = await orderService.getOrdersByUser(user?.userId);
+  //       console.log(response);
+  //       setOrders(response.data);
+  //     } catch (error) {
+  //       console.error("Error fetching orders:", error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    if (show) fetchOrders();
-  }, [show, user?.userId]);
+  //   if (show) fetchOrders();
+  // }, [show, user?.userId]);
 
   return (
     <Modal show={show} onHide={handleClose} className="profile-modal">
@@ -39,32 +39,6 @@ const ProfileModal = ({ show, handleClose, user }) => {
           <p>
             <strong>Role:</strong> {user?.userRole}
           </p>
-        </div>
-        <div className="order-history">
-          <h5>Past Orders</h5>
-          {loading ? (
-            <Spinner animation="border" />
-          ) : (
-            <ul className="order-list">
-              {orders?.map((order, index) => (
-                <li key={index} className="order-item">
-                  <p>
-                    <strong>Order ID:</strong> {order.id}
-                  </p>
-                  <p>
-                    <strong>Items:</strong> {order.items.join(", ")}
-                  </p>
-                  <p>
-                    <strong>Total:</strong> ${order.total}
-                  </p>
-                  <p>
-                    <strong>Date:</strong>{" "}
-                    {new Date(order.date).toLocaleDateString()}
-                  </p>
-                </li>
-              ))}
-            </ul>
-          )}
         </div>
       </Modal.Body>
       <Modal.Footer>
